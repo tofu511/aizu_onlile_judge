7e1ed56a2c90eb77139e124f451d29820b5a0f53
tofu511 <mokmok511@gmail.com>
2017-11-16 21:15:39 +0900

    whileを使用せず、for式で実行できるように修正

--- alds1/BubbleSort.scala
+++ alds1/BubbleSort.scala
@@ -1,35 +1,32 @@
-// AOJに提出する際はオブジェクト名をMainにすること(MainにしないとRunTime Errorになる)
-object BubbleSort {
-	def main(args: Array[String]): Unit = {
-		val scanner = new java.util.Scanner(System.in)
-		val size = scanner.nextInt()
-		var arr = Array.empty[Int]
-
-		for(i <- 0 until size) {
-			arr = arr :+ scanner.next().toInt
- 		}
-
-		bubbleSort(arr, size)
-
-		scanner.close()
-	}
-
-	def bubbleSort(a: Array[Int], n: Int): Unit = {
-		var flag = 1
-		var sortCount = 0
-		while(flag == 1) {
-			flag = 0
-			for(i <- (n - 1 until 0 by - 1)) { // マイナスにループさせる場合 by - 1 をつける
-				if(a(i-1) > a(i)) {
-					var tmp = a(i - 1)
-					a(i - 1) = a(i)
-					a(i) = tmp
-					sortCount += 1
-					flag = 1
-				}
-			}
-		}
-		println(a.mkString(" "))
-		println(sortCount)
-	} 
+// AOJに提出する際はオブジェクト名をMainにしないとRunTime Errorになる
+object Main {
+    def main(args: Array[String]): Unit = {
+        val scanner = new java.util.Scanner(System.in)
+        val length = scanner.nextInt()
+        var inputArray = Array.empty[Int]
+ 
+        for(i <- 0 until length) {
+            inputArray = inputArray :+ scanner.next().toInt
+        }
+ 
+        bubbleSort(inputArray, length)
+ 
+        scanner.close()
+ 
+    }
+ 
+    def bubbleSort(a: Array[Int], n: Int): Unit = {
+        var sortCount = 0
+        for(i <- n until 0 by - 1; j <- n - 1 until 0 by - 1) { // マイナスにループさせる場合 by - 1 をつける
+            if(a(j - 1) > a(j)) {
+                val tmp = a(j)
+                a(j) = a(j - 1)
+                a(j - 1) = tmp
+                sortCount += 1
+            }
+        }
+ 
+        println(a.mkString(" "))
+        println(sortCount)
+    }
 }
